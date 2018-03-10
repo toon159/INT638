915b4157066e80977d546b815d224c53d01ea73c
toon159 <toonbcc@gmail.com>
2018-03-10 18:11:12 +0700

    minor change

--- decision-tree.py
+++ decision-tree.py
@@ -1,3 +1,8 @@
+from sklearn.externals.six import StringIO
+from IPython.display import Image
+from sklearn.tree import export_graphviz
+import pydotplus
+
 # data analysis and wrangling
 import pandas as pd
 import numpy as np
@@ -28,14 +33,19 @@ print ("Dataset Lenght:: ", len(titanic_df))
 print ("Dataset Shape:: ", titanic_df.shape)
 print(titanic_df.columns)
 
-titanic_df = titanic_df[['Survived', 'Pclass', 'SibSp', 'Parch', 'Fare']]
+titanic_df = titanic_df[['Survived', 'Pclass', 'SibSp', 'Parch']]
 
 print(titanic_df.head())
 
-X = titanic_df.values[:, 1:9]
-Y = titanic_df.values[:,0]
+X_train = titanic_df.loc[:, 'Pclass':'Parch']
+Y_train = titanic_df.loc[:, 'Survived']
 
-X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3, random_state = 100)
+dtree = DecisionTreeClassifier(max_leaf_nodes=3, random_state=0)
+dtree.fit(X_train, Y_train)
+
+print(dtree.predict([[1,1,0]]))
+#
+# X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3, random_state = 100)
 # The above snippet will split data into training and test set.
 # X_train, y_train are training data &  X_test, y_test belongs to the test dataset.
 
@@ -43,9 +53,17 @@ X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3, rand
 # & training dataset’s size will be 70% of the entire dataset.
 # random_state variable is a pseudo-random number generator state used for random sampling.
 # If you want to replicate our results, then use the same value of random_state.
-
-
-clf_entropy = DecisionTreeClassifier(criterion = "entropy", random_state = 100,
- max_depth=3, min_samples_leaf=5)
-print(clf_entropy.fit(X_train, y_train))
+#
+#
+# clf_entropy = DecisionTreeClassifier(criterion = "entropy", random_state = 100,
+#  max_depth=3, min_samples_leaf=5)
+# print(clf_entropy.fit(X_train, y_train))
 # print(df)
+
+
+dot_data = StringIO()
+export_graphviz(dtree, out_file=dot_data,
+                filled=True, rounded=True,
+                special_characters=True)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+Image(graph.create_png())
